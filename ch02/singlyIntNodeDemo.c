84da89c0336b7e6a3ed6b58ad61ba00ae2c20bc0
edz <edz@edzdeMacBook-Air.local>
2019-11-09 19:05:02 +0800

    例题和练习

--- ch02/singlyIntNodeDemo.c
+++ ch02/singlyIntNodeDemo.c
@@ -20,22 +20,22 @@ int main(void) {
 	singlyIntNodeAdd(head, 3);
 	singlyIntNodeAdd(head, 4);
 	singlyIntNodeAdd(head, 5);
-	singlyIntNodeDisplay(head);
-	//singlyIntNodeGetLength(head);
-	//singlyIntNodeDelete(head, 3);
+	singlyIntNodeDisplay(head); //展示链表的数据
+	//singlyIntNodeGetLength(head); //获取链表的长度
+	//singlyIntNodeDelete(head, 3); //删除链表的指定位置的元素
 	//singlyIntNodeDisplay(head);
 	//singlyIntNodeGetLength(head);
 	/*singlyIntNodeDeleteElem(head, 3);
 	singlyIntNodeDisplay(head);*/
-	singlyIntNodeRevert(head);
+	singlyIntNodeRevert(head);   //链表的倒序
 	singlyIntNodeDisplay(head);
 	/*
-	singlyIntNodeInsert(head, 2, 2);
+	singlyIntNodeInsert(head, 2, 2);   //链表的插入
 	singlyIntNodeDisplay(head);
-	singlyIntNodeUpdate(head, 2, 3);
-	singlyIntNodeDisplay(head);
-	singlyIntNodeGet(head, 4);
-	*/
+	singlyIntNodeUpdate(head, 2, 3);   //链表的更新
+	singlyIntNodeDisplay(head);*/
+	singlyIntNodeGet(head, 4);   //获取链表指定位置的元素的数据
+
 
 	return 0;
 }
