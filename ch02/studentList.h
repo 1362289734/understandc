01780e5ddc0c899ffb1becd8cdc8122f321bb6e8
B17041823 黄奇嘉 <1362289734@qq.com>
2019-11-16 00:15:54 +0800

    例题与练习

--- ch02/studentList.h
+++ ch02/studentList.h
@@ -1,17 +1,17 @@
-#include "Student.h"
+﻿#include "Student.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define SIZE_STEP 10
+#define SIZE_STEP 20
 
 typedef struct StudentList {
-    // 当前存放内容的长度
-    int length;
-    // 存放数据的内存的首地址
-    Student *data;
-    // 总共分配的内存空间
-    int size;
+	// 当前存放内容的长度
+	int length;
+	// 存放数据的内存的首地址
+	Student *data;
+	// 总共分配的内存空间
+	int size;
 } StudentList;
 
 StudentList *studentListNew(int initSize);
@@ -53,7 +53,7 @@ void studentListDisplay(StudentList *list);
  * @param elem
  * @return  1操作成功，0操作失败
  */
-int studentListUpdate(StudentList *list, int index, int age, SEX sex, char *name,int num);
+int studentListUpdate(StudentList *list, int index, int age, SEX sex, char *name, int num);
 
 /**
  * 返回指定位置的元素
