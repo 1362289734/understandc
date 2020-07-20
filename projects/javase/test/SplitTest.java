7f5d51f67e6da77e3323c8e0693989da73e42242
B17041823 黄奇嘉 <1362289734@qq.com>
2020-07-20 10:13:39 +0800

    JAVASE

--- projects/javase/test/SplitTest.java
+++ projects/javase/test/SplitTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SplitTest {
     //文件的拆分和合并
@@ -32,12 +33,21 @@ public class SplitTest {
             totalSize += files[i].length();
         }
 
-        Arrays.sort(files, (o1, o2) -> {
-            String o1Name = o1.getName();
-            String o2Name = o2.getName();
-            String o1Num = o1Name.substring(o1Name.lastIndexOf("." )+1);
-            String o2Num = o2Name.substring(o1Name.lastIndexOf("." )+1);
-            return Integer.parseInt(o1Num) - Integer.parseInt(o2Num);
+//        Arrays.sort(files, (o1, o2) -> {
+//            String o1Name = o1.getName();
+//            String o2Name = o2.getName();
+//            String o1Num = o1Name.substring(o1Name.lastIndexOf("." )+1);
+//            String o2Num = o2Name.substring(o1Name.lastIndexOf("." )+1);
+//            return Integer.parseInt(o1Num) - Integer.parseInt(o2Num);
+//        });
+        Arrays.sort(files, new Comparator<File>() {
+            public int compare(File o1, File o2) {
+                String o1Name = o1.getName();
+                String o2Name = o2.getName();
+                String o1Num = o1Name.substring(o1Name.lastIndexOf("." )+1);
+                String o2Num = o2Name.substring(o1Name.lastIndexOf("." )+1);
+                return Integer.parseInt(o1Num) - Integer.parseInt(o2Num);
+            }
         });
 //        for (File file : files) {
 //            System.out.println(file.getName());
